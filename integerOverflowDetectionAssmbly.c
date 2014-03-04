1a7e23b963e58ff0b10588ecdad7e95197d79fb8
Eric Wimberley <blank89@gmail.com>
2014-03-03 20:30:35 -0500

    Create integerOverflowDetectionAssmbly.c
    Visual Studio - x86 32 bit integer overflow prevention.

---
+++ integerOverflowDetectionAssmbly.c
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+int a(){
+	int position = 0;
+	char in;
+	char vuln[10] = "dir";
+	char buff[10] = "dir";
+
+	printf("vuln position: %p\n", buff);
+	printf("buff position: %p\n", vuln);
+	
+	scanf_s("%d\n", &position);
+	if (position < 0){
+		printf("nice try...\n");
+		exit(1);
+	}
+
+	position = position * 2;
+	__asm
+	{
+		jno NO_OVERFLOW;
+		jmp abort
+		NO_OVERFLOW :
+	}
+
+	in = 0;
+	while (position < 10){
+		printf("%p\n", &vuln[position]);
+		vuln[position] = getchar();
+		position++;
+	}
+	
+	printf("vuln: %s\n", buff);
+	printf("buff: %s\n", vuln);
+	
+	system(buff);
+	return 0;
+}
+
+int _tmain(int argc, _TCHAR* argv[]){
+	a();
+	return 0;
+}
